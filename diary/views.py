493b254eaa2088a58acac96a7ea6ee68cd0c11bc
DESKTOP-2R119A6\snrn <vkdnjxodnd@naver.com>
2021-08-04 16:21:19 +0900

    page_list 페이지 추가
    diary/ 로 오면 페이지들 모아놓은 목록 볼 수 있음

--- diary/views.py
+++ diary/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Page
 
 # Create your views here.
+
+
+def page_list(request):
+    context = dict()
+    pages = Page.objects.all()
+    context["pages"] = pages
+    return render(request, 'diary/page_list.html', context)
