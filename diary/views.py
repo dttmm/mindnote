9f9464ed7171142bc10ea2a6623e103c0553e107
DESKTOP-2R119A6\snrn <vkdnjxodnd@naver.com>
2021-08-04 17:36:31 +0900

    page_detail 페이지 추가
    page_list에서 해당 리스트 누르면 상세 페이지로 이동

--- diary/views.py
+++ diary/views.py
@@ -9,3 +9,10 @@ def page_list(request):
     pages = Page.objects.all()
     context["pages"] = pages
     return render(request, 'diary/page_list.html', context)
+
+
+def page_detail(request, page_id):
+    context = dict()
+    page = Page.objects.get(id=page_id)
+    context["object"] = page
+    return render(request, 'diary/page_detail.html', context)
