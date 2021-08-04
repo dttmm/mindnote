aa24fab8a964363350d4db5fc1ab98264df008c4
DESKTOP-2R119A6\snrn <vkdnjxodnd@naver.com>
2021-08-04 16:11:25 +0900

    Page 모델 추가

--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Page(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    feelings = models.CharField(max_length=80)
+    score = models.IntegerField()
+    dt_created = models.DateField(
+        verbose_name="Date Created")
+
+    def __str__(self):
+        return self.title
