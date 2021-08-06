0c5dd49bd08d2e758bb13a819e026abd602135f1
DESKTOP-2R119A6\snrn <vkdnjxodnd@naver.com>
2021-08-06 22:20:04 +0900

    제네릭뷰의 기본값을 이용하여 뷰 간단하게 함

--- diary/urls.py
+++ diary/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     path('diary/', views.PageListView.as_view(), name='page-list'),
     path('diary/info/', views.info, name='info'),
     path('diary/write/', views.PageCreateView.as_view(), name='page-create'),
-    path('diary/page/<int:page_id>/',
+    path('diary/page/<int:pk>/',
          views.PageDetailView.as_view(), name='page-detail'),
-    path('diary/page/<int:page_id>/edit/',
+    path('diary/page/<int:pk>/edit/',
          views.PageUpdateView.as_view(), name='page-update'),
-    path('diary/page/<int:page_id>/delete/',
+    path('diary/page/<int:pk>/delete/',
          views.PageDeleteView.as_view(), name='page-delete'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -9,53 +9,43 @@ from .forms import PageForm
 # Create your views here.
 
 
-class PageListView(ListView):
-    model = Page
-    template_name = 'diary/page_list.html'
-    ordering = ['-dt_created']
-    paginate_by = 8
-    page_kwarg = 'page'
-
-
-class PageDetailView(DetailView):
-    model = Page
-    template_name = 'diary/page_detail.html'
-    pk_url_kwarg = 'page_id'
-    context_object_name = 'object'
+def index(request):
+    return render(request, 'diary/index.html')
 
 
 def info(request):
     return render(request, 'diary/info.html')
 
 
+class PageListView(ListView):
+    model = Page
+    ordering = ['-dt_created']
+    paginate_by = 8
+
+
+class PageDetailView(DetailView):
+    model = Page
+    pk_url_kwarg = 'pk'
+
+
 class PageCreateView(CreateView):
     model = Page
     form_class = PageForm
-    template_name = 'diary/page_form.html'
 
     def get_success_url(self):
-        return reverse('page-detail', kwargs={'page_id': self.object.id})
+        return reverse('page-detail', kwargs={'pk': self.object.id})
 
 
 class PageUpdateView(UpdateView):
     model = Page
     form_class = PageForm
-    template_name = 'diary/page_form.html'
-    pk_url_kwarg = 'page_id'
 
     def get_success_url(self):
-        return reverse('page-detail', kwargs={'page_id': self.object.id})
+        return reverse('page-detail', kwargs={'pk': self.object.id})
 
 
 class PageDeleteView(DeleteView):
     model = Page
-    template_name = 'diary/page_confirm_delete.html'
-    pk_url_kwarg = 'page_id'
-    context_object_name = 'page'
 
     def get_success_url(self):
         return reverse('page-list')
-
-
-def index(request):
-    return render(request, 'diary/index.html')
