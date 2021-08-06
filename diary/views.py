51a35cd9feced50fcabb6056295d56bc825d7189
DESKTOP-2R119A6\snrn <vkdnjxodnd@naver.com>
2021-08-06 09:10:52 +0900

    update기능 구현

--- diary/views.py
+++ diary/views.py
@@ -35,3 +35,15 @@ def page_create(request):
     context = dict()
     context["form"] = page_form
     return render(request, 'diary/page_form.html', context)
+
+
+def page_update(request, page_id):
+    page = Page.objects.get(id=page_id)
+    if request.method == 'POST':
+        page_form = PageForm(request.POST, instance=page)
+        if page_form.is_valid():
+            page_form.save()
+            return redirect('page-detail', page_id=page_id)
+    else:
+        page_form = PageForm(instance=page)
+    return render(request, 'diary/page_form.html', {'form': page_form})
